45f48743bccbf2c72b198536d0ec52885a451a9e
José Diaz <xoxefdp@gmail.com>
2017-06-04 01:18:00 -0500

    updated client folder structure

--- client/src/app/components/tasks/tasks.component.ts
+++ client/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { TaskService } from './../../services/task.service';
-import { Task } from './../../models/task';
+import { TaskService } from './../../../services/task.service';
+import { Task } from './../../../models/task';
 
 @Component({
   selector: 'app-tasks',
